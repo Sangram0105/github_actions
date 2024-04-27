c2c050bdb5fa2e85735501eaa775da0d247e27e8
-m <-m>
2024-04-27 20:12:27 +0530

    new repo is created

---
+++ index.js
@@ -0,0 +1,14 @@
+import * as http from 'http';
+import { say } from 'cowsay';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const output = say({ text: 'mooooo' });
+  res.end(output);
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
