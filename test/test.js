6468d59294c032ab49402156c8f986aa9d995048
-m <-m>
2024-04-28 11:45:17 +0530

    The test cases are updated

--- test/test.js
+++ test/test.js
@@ -1,14 +1,18 @@
 const request = require('supertest');
-const server = require('../index');
+const app = require('../index');
 
-describe('GET /', () => {
+describe('Server', () => {
+  afterAll(() => {
+    app.close(); // Close the server after all tests
+  });
+
   it('responds with status code 200', async () => {
-    const response = await request(server).get('/');
+    const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
   });
 
   it('responds with plain text "Hello, world!"', async () => {
-    const response = await request(server).get('/');
+    const response = await request(app).get('/');
     expect(response.text).toBe('Hello, world!\n');
   });
 });
